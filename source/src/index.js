bc9759c46762f8327fea1d863984aae656466160
Dogukan Ucak <d.ucak@tr.emakina.com>
2019-12-09 16:21:43 +0300

    Extract files from source folder to outside

--- source/src/index.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap";
-import "./assets/js/vendors/jquerySwipe";
-import "./assets/js/scrollWatcher";
-import "./assets/js/collapse";
-import "./assets/js/imagecareosel";
-import "./assets/js/overlay";
-
-import "./assets/styles/scss/main.scss";
